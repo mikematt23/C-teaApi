7e92b2e7a8aa6c90a2138ce5c2faf590dce4cd93
unknown <michaelcmatteis@gmail.com>
2025-01-15 21:28:14 -0600

    setting up db connection

---
+++ Models/TeaDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace realTeaShop.Models;
+
+public partial class TeaDbContext : DbContext
+{
+    public TeaDbContext()
+    {
+    }
+
+    public TeaDbContext(DbContextOptions<TeaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=junction.proxy.rlwy.net;port=13682;database=railway;user=root;password=JwCQadhPOMjJzdcNjazNxklSQCcoHnRl;sslmode=Preferred", Microsoft.EntityFrameworkCore.ServerVersion.Parse("9.1.0-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PRIMARY");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "email_UNIQUE").IsUnique();
+
+            entity.HasIndex(e => e.UserId, "userID_UNIQUE").IsUnique();
+
+            entity.Property(e => e.UserId).HasColumnName("userID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(225)
+                .HasColumnName("email");
+            entity.Property(e => e.Password)
+                .HasMaxLength(225)
+                .HasColumnName("password");
+            entity.Property(e => e.Score).HasColumnName("score");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace realTeaShop.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int? Score { get; set; }
+}
